ff074918a590ef8dd3069c3399a43e14b5a72790
Nicolas Haumesser <nh@axin.fr>
2020-11-10 23:11:58 +0100

    Avancement admin tiers

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,13 +1,13 @@
-export default function Table({ data, onEdit, onDelete }) {
+export default function Table({ columns, datas, onEdit, onDelete }) {
   return (
     <>
-      <div class="mb-3">
+      <div className="mb-3">
         <button
           type="button"
-          class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green active:bg-green-700 transition ease-in-out duration-150"
+          className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green active:bg-green-700 transition ease-in-out duration-150"
         >
           <svg
-            class="-ml-0.5 mr-2 h-4 w-4"
+            className="-ml-0.5 mr-2 h-4 w-4"
             width="24"
             height="24"
             viewBox="0 0 24 24"
@@ -17,91 +17,49 @@ export default function Table({ data, onEdit, onDelete }) {
             <path
               d="M12 6V12M12 12V18M12 12H18M12 12L6 12"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
           </svg>
           Créer un tiers
         </button>
       </div>
-      <div class="flex flex-col">
-        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table class="min-w-full divide-y divide-gray-200">
+      <div className="flex flex-col">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
                 <thead>
                   <tr>
-                    <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                      Name
+                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                      Nom
                     </th>
-                    <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                      Title
+                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                      Adresse
                     </th>
-                    <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                      Code postal
+                    </th>
+                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                      Ville
+                    </th>
+                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                      Tel
+                    </th>
+                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                       Email
                     </th>
-                    <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                      Role
-                    </th>
-                    <th class="px-6 py-3 bg-gray-50"></th>
-                    <th class="px-6 py-3 bg-gray-50"></th>
+                    <th className="px-6 py-3 bg-gray-50"></th>
+                    <th className="px-6 py-3 bg-gray-50"></th>
                   </tr>
                 </thead>
-                <tbody>
-                  {/* <!-- Odd row --> */}
-                  <tr class="bg-white">
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
-                      Jane Cooper
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
-                      Regional Paradigm Technician
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
-                      jane.cooper@example.com
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
-                      Admin
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
-                      <a href="#" class="text-green-600 hover:text-green-900">
-                        Editer
-                      </a>
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
-                      <a href="#" class="text-red-600 hover:text-red-900">
-                        Supprimer
-                      </a>
-                    </td>
-                  </tr>
-
-                  {/* <!-- Even row --> */}
-                  <tr class="bg-gray-50">
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
-                      Cody Fisher
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
-                      Product Directives Officer
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
-                      cody.fisher@example.com
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
-                      Owner
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
-                      <a href="#" class="text-green-600 hover:text-green-900">
-                        Editer
-                      </a>
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
-                      <a href="#" class="text-red-600 hover:text-red-900">
-                        Supprimer
-                      </a>
-                    </td>
-                  </tr>
-                  {/* <!-- More rows... --> */}
-                </tbody>
+                <TableBody
+                  columns={columns}
+                  datas={datas}
+                  onEdit={onEdit}
+                  onDelete={onDelete}
+                />
               </table>
             </div>
           </div>
@@ -109,4 +67,20 @@ export default function Table({ data, onEdit, onDelete }) {
       </div>
     </>
   );
+}
+
+function TableBody({ columns, datas, onEdit, onDelete }) {
+  //data[column.name]
+  const body = datas.map((data) => {
+    const rows = columns.map((column) => {
+      return (
+        <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
+          {data[column.key]}
+        </td>
+      );
+    });
+    return <tr className="bg-white">{rows}</tr>;
+  });
+
+  return <tbody>{body}</tbody>;
 }
--- src/pages/admin/tiers.js
+++ src/pages/admin/tiers.js
@@ -1,7 +1,18 @@
 import Table from "../../components/Table";
 import AdminLayout from "../../layouts/AdminLayout";
+import { db } from "../../prisma";
+import { toJson } from "../../tools/tools";
 
-export default function TierPage() {
+const columns = [
+  { key: "raisonSociale" },
+  { key: "adresse" },
+  { key: "codePostal" },
+  { key: "ville" },
+  { key: "tel" },
+  { key: "email" },
+];
+
+export default function TierPage({ tiers }) {
   return (
     <AdminLayout>
       <div className="pt-10 pb-2">
@@ -18,7 +29,7 @@ export default function TierPage() {
           <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div className="py-6">
               <div className="mt-8">
-                <Table />
+                <Table columns={columns} datas={tiers} />
               </div>
             </div>
           </div>
@@ -26,4 +37,14 @@ export default function TierPage() {
       </div>
     </AdminLayout>
   );
+}
+
+export async function getServerSideProps() {
+  const tiers = await db.tiers.findMany();
+  const dataTiers = toJson(tiers);
+  return {
+    props: {
+      tiers: dataTiers,
+    },
+  };
 }
