be32b66a0c266c93ae02930a6e1042d53d1580b5
Julien CAVALEIRO <julien@cavaleiro.fr>
2020-11-10 22:23:44 +0100

    correction de l'icone pour la page active dans le layout admin

--- src/layouts/AdminLayout.js
+++ src/layouts/AdminLayout.js
@@ -5,7 +5,6 @@ import { useMemo, useState } from "react";
 
 function AdminLink({ href, className, activeClassName, children }) {
   const router = useRouter();
-
   let isActive = router.pathname === href;
 
   return (
@@ -18,6 +17,23 @@ function AdminLink({ href, className, activeClassName, children }) {
         {children}
       </a>
     </Link>
+  );
+}
+
+function SvgLink({ href, activeClassName, className, ...props }) {
+  const router = useRouter();
+  let isActive = router.pathname === href;
+
+  return (
+    <svg
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      {...props}
+      className={isActive ? activeClassName : className}
+    />
   );
 }
 
@@ -41,33 +57,24 @@ export default function AdminLayout({ children }) {
         href: "/",
         label: "Retour à l'accueil",
         icon: (
-          <svg
-            className="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500 transition ease-in-out duration-150"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
+          <SvgLink className="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500 transition ease-in-out duration-150">
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
               d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
             />
-          </svg>
+          </SvgLink>
         ),
       },
       {
         href: "/admin",
         label: "Calendrier",
         icon: (
-          <svg
+          <SvgLink
+            href="/admin"
             className="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500 transition ease-in-out duration-150"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+            activeClassName="mr-3 h-6 w-6 text-purple-500 transition ease-in-out duration-150"
           >
             <path
               stroke-linecap="round"
@@ -75,20 +82,17 @@ export default function AdminLayout({ children }) {
               stroke-width="2"
               d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
             />
-          </svg>
+          </SvgLink>
         ),
       },
       {
         href: "/admin/tiers",
         label: "Tiers",
         icon: (
-          <svg
+          <SvgLink
+            href="/admin/tiers"
             className="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500 transition ease-in-out duration-150"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+            activeClassName="mr-3 h-6 w-6 text-purple-500 transition ease-in-out duration-150"
           >
             <path
               strokeLinecap="round"
@@ -96,19 +100,17 @@ export default function AdminLayout({ children }) {
               strokeWidth={2}
               d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
             />
-          </svg>
+          </SvgLink>
         ),
       },
       {
         href: "/admin/posts",
         label: "Billet",
         icon: (
-          <svg
+          <SvgLink
+            href="/admin/posts"
             className="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500 transition ease-in-out duration-150"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+            activeClassName="mr-3 h-6 w-6 text-purple-500 transition ease-in-out duration-150"
           >
             <path
               stroke-linecap="round"
@@ -116,20 +118,17 @@ export default function AdminLayout({ children }) {
               stroke-width="2"
               d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
             />
-          </svg>
+          </SvgLink>
         ),
       },
       {
         href: "/admin/theme",
         label: "Themes",
         icon: (
-          <svg
-            className="mr-3 h-6 w-6 text-purple-500 transition ease-in-out duration-150"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+          <SvgLink
+            href="/admin/theme"
+            className="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500 transition ease-in-out duration-150"
+            activeClassName="mr-3 h-6 w-6 text-purple-500 transition ease-in-out duration-150"
           >
             <path
               stroke-linecap="round"
@@ -143,7 +142,7 @@ export default function AdminLayout({ children }) {
               stroke-width="2"
               d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
             />
-          </svg>
+          </SvgLink>
         ),
       },
     ];
